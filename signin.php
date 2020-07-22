d9bccd337c54861b22962a6c91ef6a6f0c2680bd
wiiinnaa <wiiinnaa@gmail.com>
2020-07-21 21:05:40 -0500

    inicio de seccion

---
+++ signin.php
@@ -0,0 +1,33 @@
+<?php 
+    include("conexion.php");
+
+    if(isset($_POST['user']) && isset($_POST['pass']))
+    {
+        $user = mysql_fix_string($conexion, $_POST['user']);
+        $pass = md5($_POST['pass']);
+
+        $query = "SELECT * FROM users WHERE user='$user' AND pass='$pass'";
+
+        $result = $conexion->query($query);
+        if ($result->num_rows >= 1)
+            header("Location: notas.php");    
+        else
+        {
+            /*echo "Usuario o password incorrecto <a href='signup.php'>Registrarse?</a>"; 
+            echo "<br><a href='signin.php'>Ingresar</a>";*/
+            $_SESSION['message'] = 'fallo inicio de sesion Registrese';
+            $_SESSION['message_type'] = 'warning';
+            header("Location: signin.php");
+        }
+    }
+    else
+    {
+        header("Location: index.php");
+    }
+    function mysql_fix_string($coneccion, $string)
+    {
+        if (get_magic_quotes_gpc())
+            $string = stripcslashes($string);
+        return $coneccion->real_escape_string($string);
+    }
+?>
